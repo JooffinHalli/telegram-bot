4d1bd9ff38604389be87ac3bc1263bcfdc78c27c
Бойко Артем <boiko.a@MacBook-Pro-Bojko.local>
2025-03-24 18:07:46 +0300

    comm

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-require('dotenv').config({ path: './.env' });
+var main = () => {
+  require('dotenv').config({ path: './.env' });
 
-const { Telegraf } = require('telegraf');
-const { sendMessage } = require('./gpt');
+  const { Telegraf } = require('telegraf');
+  const { sendMessage } = require('./gpt');
 
-const bot = new Telegraf(process.env.TELEGRAM_KEY);
+  const bot = new Telegraf(process.env.TELEGRAM_KEY);
 
-bot.on('message', (ctx) => {
-  sendMessage(ctx.message.text).then(
-    (res) => ctx.reply(res),
-    (reason) => ctx.reply(reason)
-  );
-});
+  bot.on('message', (ctx) => {
+    sendMessage(ctx.message.text).then(
+      (res) => ctx.reply(res),
+      (reason) => ctx.reply(reason)
+    );
+  });
 
-bot.launch();
+  bot.launch();
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
+
+main();
+
+module.exports = main;
