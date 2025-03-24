3ca7f9d8d573ddffce5f4a833bcfc6f6aa2f2136
Бойко Артем <boiko.a@MacBook-Pro-Bojko.local>
2025-03-24 18:37:38 +0300

    comm

--- index.mjs
+++ index.mjs
@@ -4,20 +4,18 @@ dotenv.config({ path: './.env' });
 import { Telegraf } from 'telegraf';
 import { sendMessage } from './gpt.mjs';
 
-export var main = () => {
-    var bot = new Telegraf(process.env.TELEGRAM_KEY);
+var bot = new Telegraf(process.env.TELEGRAM_KEY);
 
-    bot.on('message', (ctx) => {
-        sendMessage(ctx.message.text).then(
-            (res) => ctx.reply(res),
-            (reason) => ctx.reply(reason)
-        );
-    });
+bot.on('message', (ctx) => {
+    sendMessage(ctx.message.text).then(
+        (res) => ctx.reply(res),
+        (reason) => ctx.reply(reason)
+    );
+});
 
-    bot.launch();
+bot.launch();
 
-    process.once('SIGINT', () => bot.stop('SIGINT'));
-    process.once('SIGTERM', () => bot.stop('SIGTERM'));
-}
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-main();
+module.exports = bot;
